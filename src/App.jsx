54d5846d713c76b75b5a27af3b472ddacfb012ec
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-18 11:09:24 +0200

    login code on login page

--- src/App.jsx
+++ src/App.jsx
@@ -117,62 +117,10 @@ function App() {
 						element={<PageJobApplications />}
 					/>
 					<Route path="/cv" element={<PageCv />} />
-					<Route path="/login" element={<PageLogin />} />
+					<Route path="/login" element={<PageLogin message={message} jobSources={jobSources} userIsLoggedIn={userIsLoggedIn} currentUser={currentUser} currentUserIsInAccessGroup={currentUserIsInAccessGroup} handleLogoutButton={handleLogoutButton} handleLoginButton={handleLoginButton} username={username} password={password} setUsername={setUsername} setPassword={setPassword} />} />
 					<Route path="/register" element={<PageRegister />} />
 				</Routes>
 
-				<div className="loggedInInfo">
-					{userIsLoggedIn() && (
-						<div>
-							Logged in: {currentUser.firstName}{' '}
-							{currentUser.lastName}
-						</div>
-					)}
-				</div>
-				<div className="info">
-					{currentUserIsInAccessGroup('administrators') && (
-						<div>info for administrators</div>
-					)}
-					{currentUserIsInAccessGroup('jobSeekers') && (
-						<div>new job information for job seekers</div>
-					)}
-				</div>
-				{userIsLoggedIn() ? (
-					<>
-						<p>There are {jobSources.length} job sources:</p>
-						<ul>
-							{jobSources.map((jobSource, i) => {
-								return <li key={i}>{jobSource.name}</li>;
-							})}
-						</ul>
-						<button className="logout" onClick={handleLogoutButton}>
-							Logout
-						</button>
-					</>
-				) : (
-					<form className="login" onSubmit={handleLoginButton}>
-						<div className="row">
-							username:{' '}
-							<input
-								onChange={(e) => setUsername(e.target.value)}
-								value={username}
-								type="text"
-							/>
-						</div>
-						<div className="row">
-							password:{' '}
-							<input
-								onChange={(e) => setPassword(e.target.value)}
-								value={password}
-								type="password"
-							/>
-						</div>
-						<div className="row">
-							<button>Login</button>
-						</div>
-						<div className="row">{message}</div>
-					</form>
-				)}
 			</>
 		</div>
 	);
--- src/pages/PageLogin.jsx
+++ src/pages/PageLogin.jsx
@@ -1,7 +1,59 @@
-export const PageLogin = () => {
+export const PageLogin = ({message, jobSources, userIsLoggedIn, currentUser, currentUserIsInAccessGroup, handleLogoutButton, handleLoginButton, username, password, setUsername, setPassword}) => {
 	return (
 		<>
 			<p>This is the Login page.</p>
+			<div className="loggedInInfo">
+				{userIsLoggedIn() && (
+					<div>
+						Logged in: {currentUser.firstName}{' '}
+						{currentUser.lastName}
+					</div>
+				)}
+			</div>
+			<div className="info">
+				{currentUserIsInAccessGroup('administrators') && (
+					<div>info for administrators</div>
+				)}
+				{currentUserIsInAccessGroup('jobSeekers') && (
+					<div>new job information for job seekers</div>
+				)}
+			</div>
+			{userIsLoggedIn() ? (
+				<>
+					<p>There are {jobSources.length} job sources:</p>
+					<ul>
+						{jobSources.map((jobSource, i) => {
+							return <li key={i}>{jobSource.name}</li>;
+						})}
+					</ul>
+					<button className="logout" onClick={handleLogoutButton}>
+						Logout
+					</button>
+				</>
+			) : (
+				<form className="login" onSubmit={handleLoginButton}>
+					<div className="row">
+						username:{' '}
+						<input
+							onChange={(e) => setUsername(e.target.value)}
+							value={username}
+							type="text"
+						/>
+					</div>
+					<div className="row">
+						password:{' '}
+						<input
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+							type="password"
+						/>
+					</div>
+					<div className="row">
+						<button>Login</button>
+					</div>
+					<div className="row">{message}</div>
+				</form>
+			)}
 		</>
 	);
 };
